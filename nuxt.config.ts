7e02cd85a705a7887bec668815da2cebef580fc0
Lherner Remon <lherner21@gmail.com>
2023-11-04 21:02:02 -0500

    feat: deactivate ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
   telemetry: false,
   css: ['assets/main.scss'],
   // enable takeover mode
